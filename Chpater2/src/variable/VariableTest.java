e5e3b2ea8d7295ae02b2d01304ec28e983725b41
권지현 <dial2825@naver.com>
2021-03-18 23:24:27 +0900

    no message

---
+++ Chpater2/src/variable/VariableTest.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		int age; // 주로 변수 선언 시 노란줄이 나온다. 
+				 //변수를 사용 안했을 시
+		
+		age = 30; // 값 대입  30이 age에 들어간다.
+		
+		System.out.println(age); //항상 값을 대입 후 출력
+		
+		int numberOfStudent; //camel notation
+		
+	}
+
+}
+/* 변수 이름은 영문자나 숫자를 사용함. 단 숫자로  시작할 수 없음
+   특수문자 _ $ 가능 
+   예약어 사용 불가 
+  변수는 그 쓰임에 맞는 이름으로 명명 
+ */
---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) { //main은 시작점을 위한 함수 
+											 //실행하기 위해 사용하는 함수
+		
+		System.out.println("Hello, Java"); //출력을 위한 구문 
+
+	}
+	
+	
+
+}
