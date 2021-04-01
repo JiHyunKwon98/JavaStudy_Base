d79b6318f086f8bea044dc1b43d16f2fe92ee89c
권지현 <dial2825@naver.com>
2021-04-02 00:11:54 +0900

    no message

---
+++ Chapter7/src/polymorphism/Customer.java
@@ -0,0 +1,85 @@
+package polymorphism;
+/*상속을 사용하여 고객관리 프로그램 구현 
+ *  고객의 등급에 따라 차별화된 서비스를 제공할 수 있다.
+ *  고객의 등급에 따라 할인률, 적립금이 다르게 적용된다.*/ 
+
+public class Customer {
+
+	protected int customerID; //고객 아이디
+	protected String customerName; //고객 이름
+	protected String customerGrade; //고객 등급 (기본 생서자에서 지정되는 기본 등급은 SILVER) 
+	int bonusPoint; //고객 보너스 포인트 (고객이 제품을 구매하는 경우 누적되는 보너스 포인트 )
+	double bonusRatio; // 보너스 포인트 적립 비용 (고객이 제품을 구매할 때 구매 금액의 일정 비율이 보너스
+					   // 트로 적립. 이 때 계산되는 적립 비용
+					   // 기본 생성자에서 지정되는 적립 비율은 1%. 
+				       //즉 10,000원 짜리를 사면 100원이 적립
+/*
+	public Customer() { // 기본 생성자 
+		customerGrade = "SILVER"; //멤버변수 초기화 
+		bonusRatio = 0.01; 
+		
+		System.out.println("Customer() 생성자 호출");
+	}
+*/	
+	public Customer(int customerID, String customerName) {
+		this.customerID = customerID;
+		this.customerName = customerName;
+		customerGrade = "SILVER"; //멤버변수 초기화 
+		bonusRatio = 0.01; 
+		
+		//System.out.println("Customer(int, String) 생성자 호출");
+
+	}
+	
+	public int calcPrice(int price) { //가격 계산하는 메서드 
+		bonusPoint += price * bonusRatio;  //보너스포인트 적립 
+		return price;//가격을 받은 후 지불할 가격을 반환
+	}
+	
+	public String showCustomerInfo() {
+		return customerName  + "님의 등급은" + customerGrade+"이며, 적립된 보너스 포인트는"+ bonusPoint+"점 입니다.";
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}
+
+	public int getBonusPoint() {
+		return bonusPoint;
+	}
+
+	public void setBonusPoint(int bonusPoint) {
+		this.bonusPoint = bonusPoint;
+	}
+
+	public double getBonusRatio() {
+		return bonusRatio;
+	}
+
+	public void setBonusRatio(double bonusRatio) {
+		this.bonusRatio = bonusRatio;
+	}
+	
+	
+	
+}
---
+++ Chapter7/src/polymorphism/VIPCustomer.java
@@ -0,0 +1,45 @@
+package polymorphism;
+/*
+ * Customer를 상속받아 구현하는 VIPCustomer 클래스
+ * VIPCustomer 클래스의 기능
+ * 단골고객으로 혜택이 더 다양해짐
+ * 제품 구매시 10% 할인
+ * 보너스 포인트 5%적립
+ * 담당 상담원 배정
+ * Customer class와 유사하지만, 그보다 더 많은 속성과 기능이 필요 
+*/
+public class VIPCustomer extends Customer {
+	
+	double salesRatio;
+	private int agentID;
+	
+	/*
+	public VIPCustomer() {
+		
+		super(); //상위 클래스의 참조값을 갖고 있음  상위클래스의 기본생성자 호출
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.01;
+		
+		System.out.println("VIPCustomer() 생성자 호출");
+	}*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		
+		super(customerID, customerName); 
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.01;
+		
+		//System.out.println("VIPCustomer() 생성자 호출");
+	}
+
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price*bonusRatio;
+		return price - (int)(price*salesRatio);
+	}
+	
+	
+	
+}
