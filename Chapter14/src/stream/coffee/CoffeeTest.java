45e6bb05f3764b25b5e4b41c2b3e4d788e1eda5a
권지현 <dial2825@naver.com>
2021-04-17 08:55:48 +0900

    no message

---
+++ Chapter14/src/stream/coffee/CoffeeTest.java
@@ -0,0 +1,21 @@
+package stream.coffee;
+
+public class CoffeeTest {
+
+	public static void main(String[] args) {
+		
+		Coffee americano = new KenyaAmericano();
+		americano.brewing();
+		System.out.println();
+		
+		Coffee kenyaLatte = new Latte(new KenyaAmericano());
+		kenyaLatte.brewing();
+		System.out.println();
+		
+		Coffee kenyaMocha = new Mocha(new KenyaAmericano());
+		kenyaMocha.brewing();
+		
+		
+	}
+
+}
